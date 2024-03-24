bb5353168256c816d0c96b4b8b12e6255b4bd47f
Rodrigo Castro <160682285+rodrigopcastro018@users.noreply.github.com>
2024-03-24 03:07:07 -0300

    test job summary creation

---
+++ .github/fast_summary.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+def get_test_results():
+
+    with open('test_results/mypy_result.txt', 'r') as mypy_result_file:
+        # Remove null bytes (\x00) and extra newline character (\n)
+        full_mypy_result = mypy_result_file.read().replace('\x00', '')
+        full_mypy_result = full_mypy_result.replace('\n\n', '\n')
+
+        # Get mypy result
+        mypy_result = full_mypy_result.split('\n')[-2]
+
+        if 'no issues found' in mypy_result:
+            is_mypy_successful = True
+        else:
+            is_mypy_successful = False
+
+    with open('test_results/pytest_result.txt', 'r') as pytest_result_file:
+        # Remove null bytes (\x00) and extra newline character (\n)
+        full_pytest_result = pytest_result_file.read().replace('\x00', '')
+        full_pytest_result = full_pytest_result.replace('\n\n', '\n')
+        full_pytest_result_splitted = full_pytest_result.split('\n')
+
+        # Get pytest result
+        pytest_result = full_pytest_result_splitted[-2]
+
+        if 'failed' in pytest_result:
+            is_pytest_successful = False
+        else:
+            is_pytest_successful = True
+
+        # Get coverage value
+        coverage_value = full_pytest_result_splitted[-4].split()[-1]
+
+    return is_mypy_successful, is_pytest_successful, coverage_value
+
+def write_test_results():
+
+    (is_mypy_successful,
+     is_pytest_successful,
+     coverage_value) = get_test_results()
+    
+
+
+# | **Case** | mypy | pytest | coverage |
+# | :------- | :--: | :----: | :------: |
+
+# | {} | {} | {} | {} |
+
+# "| **Case** | mypy | pytest | coverage |\n| :------- | :--: | :----: | :------: |\n|    a     |  b   |   c    |    d     |"
